0ce1e13a05873a8a53ea5ee1f7b2f372a12c1be0
Beat Küng <beat-kueng@gmx.net>
2014-07-21 14:23:27 +0200

    + timer: add timeout methods (which handle wrap around)

--- kernel/timer.h
+++ kernel/timer.h
@@ -34,6 +34,56 @@ extern "C" {
 #define delay(ms) udelay((ms)*1000)
 
 
+/* from the linux kernel... use with unsigned variables! */
+/*
+ *	These inlines deal with timer wrapping correctly. You are 
+ *	strongly encouraged to use them
+ *	1. Because people otherwise forget
+ *	2. Because if the timer wrap changes in future you won't have to
+ *	   alter your driver code.
+ *
+ * time_after(a,b) returns true if the time a is after time b.
+ *
+ * Do this with "<0" and ">=0" to only test the sign of the result. A
+ * good compiler would generate better code (and a really good compiler
+ * wouldn't care). Gcc is currently neither.
+ */
+#define time_after(a,b)		\
+	 (((int)((b) - (a)) < 0))
+#define time_before(a,b)	time_after(b,a)
+
+#define time_after_eq(a,b)	\
+	 (((int)((a) - (b)) >= 0))
+#define time_before_eq(a,b)	time_after_eq(b,a)
+
+/*
+ * Calculate whether a is in the range of [b, c].
+ */
+#define time_in_range(a,b,c) \
+	(time_after_eq(a,b) && \
+	 time_before_eq(a,c))
+
+/*
+ * Calculate whether a is in the range of [b, c).
+ */
+#define time_in_range_open(a,b,c) \
+	(time_after_eq(a,b) && \
+	 time_before(a,c))
+
+
+
+/**
+ * initialize a timeout variable called name, initialized to now+timeout_usec
+ */
+#define timeout_init(name, timeout_usec) \
+	uint32 name = getTimestamp() + (timeout_usec)
+
+/**
+ * check for a timeout using a name initialized with timeout_init
+ */
+#define timed_out(name) \
+	time_after(getTimestamp(), name)
+
 #ifdef __cplusplus
 }
 #endif
