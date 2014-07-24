7f93cc5e196c35efe97c86483794f179693dcae9
Beat Küng <beat-kueng@gmx.net>
2014-07-24 13:33:22 +0200

    add helper class DeltaTime to measure past time

---
+++ kernel/aux/delta_time.hpp
@@ -0,0 +1,51 @@
+/*
+ * Copyright (C) 2014 Beat Küng <beat-kueng@gmx.net>
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 3 of the License.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ */
+
+#ifndef _DELTA_TIME_HEADER_HPP_
+#define _DELTA_TIME_HEADER_HPP_
+
+#include <kernel/timer.h>
+
+/**
+ * helper class to calculate delta times
+ */
+class DeltaTime {
+public:
+	DeltaTime() { m_last_timestamp = getTimestamp(); }
+	
+	/**
+	 * @return microseconds since last delta call (or since object constructed)
+	 */
+	uint nextDeltaMicro() {
+		uint timestamp = getTimestamp();
+		uint dt = (timestamp - m_last_timestamp);
+		m_last_timestamp = timestamp;
+		return dt;
+	}
+	
+	/**
+	 * same as nextDeltaMicro but in milliseconds
+	 */
+	template<typename T>
+	T nextDeltaMilli() {
+		return (T)nextDeltaMicro()/(T)1000;
+	}
+private:
+	uint m_last_timestamp;
+};
+
+
+#endif /* _DELTA_TIME_HEADER_HPP_ */
+
+
